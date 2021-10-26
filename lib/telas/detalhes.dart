e1511edd55bcced5595abddef632d55ca175b092
Angelica dos Santos <65343425+angelcomp@users.noreply.github.com>
2021-10-26 10:44:37 -0300

    acesso à tela de detalhes, imagem de background

--- lib/telas/detalhes.dart
+++ lib/telas/detalhes.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lojinha_alura/models/movel.dart';
 import 'package:lojinha_alura/widgets/appbar_customizada.dart';
 
 class Detalhes extends StatelessWidget {
+  final Movel movel;
+
+  Detalhes({required this.movel});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarCustomizada(titulo: 'Detalhes', ehPaginaCarrinho: false),
-      body: ElevatedButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/carrinho');
-        },
-        child: Text('Ir para carrinho'),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('utilidades/assets/imagens/${movel.foto}'),
+              fit: BoxFit.cover
+        )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar:
+            AppBarCustomizada(titulo: movel.titulo, ehPaginaCarrinho: false),
+        body: Text('Ir para carrinho'),
       ),
     );
   }
--- lib/widgets/elemento_grid_produtos.dart
+++ lib/widgets/elemento_grid_produtos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lojinha_alura/models/movel.dart';
+import 'package:lojinha_alura/telas/detalhes.dart';
 import 'package:lojinha_alura/widgets/degrade_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/imagem_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/titulo_elemento_grid_produtos.dart';
@@ -12,25 +13,30 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [BoxShadow(
-         spreadRadius: 2,
-         blurRadius: 8,
-         color: Colors.black12
-        ),
-        ]
-      ),
-      margin: EdgeInsets.all(8),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            ImagemElementoGridProdutos(imagem: movel.foto),
-            DegradeElementoGridProdutos(),
-            TituloElementoGridProdutos(titulo: movel.titulo),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Detalhes(movel: movel),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(spreadRadius: 2, blurRadius: 8, color: Colors.black12),
+        ]),
+        margin: EdgeInsets.all(8),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              ImagemElementoGridProdutos(imagem: movel.foto),
+              DegradeElementoGridProdutos(),
+              TituloElementoGridProdutos(titulo: movel.titulo),
+            ],
+          ),
         ),
       ),
     );
