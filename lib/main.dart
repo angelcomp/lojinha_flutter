fe490756c2771ea83b7369b436956c981d565437
Angelica dos Santos <65343425+angelcomp@users.noreply.github.com>
2021-10-26 09:54:40 -0300

    titulo no topo, divider, margens

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/carrinho': (context) => Carrinho()
-      },
+      routes: {'/carrinho': (context) => Carrinho()},
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -84,7 +81,30 @@ class Inicio extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBarCustomizada(titulo: 'Lojinha', ehPaginaCarrinho: false),
-      body: GridProdutos(moveis: moveis)
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin:
+                      EdgeInsets.only(left: 30, right: 20, top: 10, bottom: 10),
+                  child: Divider(),
+                ),
+              ),
+              Text('Produtos'),
+              Expanded(
+                child: Container(
+                  margin:
+                      EdgeInsets.only(left: 20, right: 30, top: 10, bottom: 10),
+                  child: Divider(),
+                ),
+              ),
+            ],
+          ),
+          Flexible(child: GridProdutos(moveis: moveis))
+        ],
+      ),
     );
   }
 }
