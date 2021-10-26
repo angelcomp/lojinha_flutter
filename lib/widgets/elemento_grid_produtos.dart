0280aaf6ea8f7178b90b8b605b98748b1edcd6c6
Angelica dos Santos <65343425+angelcomp@users.noreply.github.com>
2021-10-26 09:29:37 -0300

    adicionando borda e sombra ao card

--- lib/widgets/elemento_grid_produtos.dart
+++ lib/widgets/elemento_grid_produtos.dart
@@ -1,29 +1,38 @@
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:lojinha_alura/models/movel.dart';
 import 'package:lojinha_alura/widgets/degrade_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/imagem_elemento_grid_produtos.dart';
 import 'package:lojinha_alura/widgets/titulo_elemento_grid_produtos.dart';
 
 class ElementoGridProdutos extends StatelessWidget {
-
   late final Movel movel;
 
   ElementoGridProdutos({required this.movel});
 
   @override
   Widget build(BuildContext context) {
-      return Stack(
-        alignment: Alignment.center,
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [BoxShadow(
+         spreadRadius: 2,
+         blurRadius: 8,
+         color: Colors.black12
+        ),
+        ]
+      ),
+      margin: EdgeInsets.all(8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          alignment: Alignment.center,
           children: [
             ImagemElementoGridProdutos(imagem: movel.foto),
             DegradeElementoGridProdutos(),
             TituloElementoGridProdutos(titulo: movel.titulo),
           ],
-
-      );
-
+        ),
+      ),
+    );
   }
-
 }
